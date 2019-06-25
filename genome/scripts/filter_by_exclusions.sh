d6e709560a5e3e6f1f7451517078fd8d11ac1414
rpolicastro <31133814+rpolicastro@users.noreply.github.com>
2019-06-25 15:17:16 -0400

    redoing filtered genes with new exclusions list

--- genome/scripts/filter_by_exclusions.sh
+++ genome/scripts/filter_by_exclusions.sh
@@ -2,7 +2,7 @@
 
 ## get excluded genes
 
-EXCLUSIONS=($(awk 'NR>1{print $1}' ../biomart/results/genes_to_exclude.tsv))
+EXCLUSIONS=($(awk 'NR>1{print $1}' ../../biomart/results/genes_to_exclude.tsv))
 
 ## turn excluded genes to regexp
 
@@ -10,6 +10,6 @@ REGEXP=$(echo ${EXCLUSIONS[@]} | tr " " "|")
 
 ## filter GTF using regexp
 
-mkdir -p ./filtered_gtfs
+mkdir -p ../filtered_gtfs
 
 grep -vE "$REGEXP" ../Saccharomyces_cerevisiae.R64-1-1.96.gtf > ../filtered_gtfs/Saccharomyces_cerevisiae.R64-1-1.96.Filtered.gtf
