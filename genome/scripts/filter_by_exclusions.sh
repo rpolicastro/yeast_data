b805b15fe091748e5e457c622785aa7fb2e9108a
rpolicastro <31133814+rpolicastro@users.noreply.github.com>
2019-06-26 15:37:47 -0400

    adding GTF with protein coding gene

--- genome/scripts/filter_by_exclusions.sh
+++ genome/scripts/filter_by_exclusions.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+## Filter based on biomart exclusions
+## ----------
+
 ## get excluded genes
 
 EXCLUSIONS=($(awk 'NR>1{print $1}' ../../biomart/results/genes_to_exclude.tsv))
@@ -13,3 +16,14 @@ REGEXP=$(echo ${EXCLUSIONS[@]} | tr " " "|")
 mkdir -p ../filtered_gtfs
 
 grep -vE "$REGEXP" ../Saccharomyces_cerevisiae.R64-1-1.96.gtf > ../filtered_gtfs/Saccharomyces_cerevisiae.R64-1-1.96.Filtered.gtf
+
+## make GTFs based on annotation type
+## ----------
+
+BIOTYPES=(protein_coding ncRNA psuedogene rRNA snRNA snoRNA tRNA)
+
+for BIOTYPE in ${BIOTYPES[@]};
+do
+	grep -E "^\#\!" ../Saccharomyces_cerevisiae.R64-1-1.96.gtf > ../filtered_gtfs/Saccharomyces_cerevisiae.R64-1-1.96.${BIOTYPE}.gtf
+	grep -E "gene_biotype \"${BIOTYPE}\"" ../Saccharomyces_cerevisiae.R64-1-1.96.gtf >> ../filtered_gtfs/Saccharomyces_cerevisiae.R64-1-1.96.${BIOTYPE}.gtf
+done
