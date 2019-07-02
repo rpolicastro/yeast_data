cbaa3ac07eff3cfebda777320b180abafe0dff9a
rpolicastro <31133814+rpolicastro@users.noreply.github.com>
2019-07-02 13:33:37 -0400

    adding filter for dubious open reading frames

--- genome/scripts/filter_by_exclusions.sh
+++ genome/scripts/filter_by_exclusions.sh
@@ -27,3 +27,18 @@ do
 	grep -E "^\#\!" ../Saccharomyces_cerevisiae.R64-1-1.96.gtf > ../filtered_gtfs/Saccharomyces_cerevisiae.R64-1-1.96.${BIOTYPE}.gtf
 	grep -E "gene_biotype \"${BIOTYPE}\"" ../Saccharomyces_cerevisiae.R64-1-1.96.gtf >> ../filtered_gtfs/Saccharomyces_cerevisiae.R64-1-1.96.${BIOTYPE}.gtf
 done
+
+## Filter mitochondrial genes
+## ----------
+
+awk 'NR<=5 || $1 == "Mito"' ../Saccharomyces_cerevisiae.R64-1-1.96.gtf > \
+../filtered_gtfs/Saccharomyces_cerevisiae.R64-1-1.96.Mitochondrial.gtf
+
+## Filter dubious open reading frames
+## ----------
+
+DUBIOUS=($(grep -E "Dubious open reading frame" ../../biomart/results/gene_master_list.tsv | cut -f1))
+REGEX=$(echo ${DUBIOUS[@]} | tr " " "|")
+
+grep -E "^\#\!" ../Saccharomyces_cerevisiae.R64-1-1.96.gtf > ../filtered_gtfs/Saccharomyces_cerevisiae.R64-1-1.96.Dubious_ORFs.gtf
+grep -E "$REGEX" ../Saccharomyces_cerevisiae.R64-1-1.96.gtf >> ../filtered_gtfs/Saccharomyces_cerevisiae.R64-1-1.96.Dubious_ORFs.gtf
